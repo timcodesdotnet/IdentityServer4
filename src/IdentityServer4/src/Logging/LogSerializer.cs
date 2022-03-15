7137bfe20a2b09beffd962e19f83c3aac5d471dd
TimCodes.NET <timcodes.net@gmail.com>
2022-03-15 14:42:41 +0000

    License obligations

--- src/IdentityServer4/src/Logging/LogSerializer.cs
+++ src/IdentityServer4/src/Logging/LogSerializer.cs
@@ -1,6 +1,6 @@
 // Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
-
+// Modified by TimCodes.NET
 
 using System.Text.Json;
 using System.Text.Json.Serialization;
